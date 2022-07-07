4785c5ff4df2595bb2a249854a4bd7d6f55fbf43
Matthew <matthew.rl.trent@gmail.com>
2022-07-07 15:32:54 -0700

    fixed route name 'useres' -> 'users'

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const authenticateToken = require("../middlewares/authenticateToken");
 const { users, universities } = require("../controllers/searchController");
 
-router.post("/useres", authenticateToken, users);
+router.post("/users", authenticateToken, users);
 
 router.post("/universities", authenticateToken, universities);
 
