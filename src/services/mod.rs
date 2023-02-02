bf0f23a58c902f2d274cf7a19fd97b80c4c26ef6
Matthew <matthew.rl.trent@gmail.com>
2023-02-02 03:54:14 -0800

    add gitignore; WIP: /hottest route

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,37 +1,19 @@
 pub mod auth;
 pub mod posts;
 
-use actix_web::{
-	HttpRequest,
-	get,
-};
 use actix_web::web;
+use actix_web::{get, HttpRequest};
 use futures::TryStreamExt;
-use log::{
-	debug,
-	error,
-	info,
-	warn,
-};
+use log::{debug, error, info, warn};
 use maxminddb::geoip2;
-use mongodb::Database;
-use mongodb::bson::{
-	Document,
-	doc,
-};
 use mongodb::bson::document::ValueAccessError;
+use mongodb::bson::{doc, Document};
+use mongodb::Database;
 use serde::Serialize;
 
-use crate::{
-	GeoIpReader,
-	to_unexpected,
-};
-use crate::api_types::{
-	ApiResult,
-	Failure,
-	success,
-};
+use crate::api_types::{success, ApiResult, Failure};
 use crate::types::School;
+use crate::{to_unexpected, GeoIpReader};
 
 #[derive(Serialize)]
 pub struct SchoolListing {
@@ -103,12 +85,11 @@ pub async fn schools_list(
 				Ok(SchoolListing {
 					id: String::from(doc.get_str("_id")?),
 					name: String::from(doc.get_str("name")?),
-					distance:
-						if doc.is_null("distance") {
-							None
-						} else {
-							Some(doc.get_f64("distance")? as f32)
-						},
+					distance: if doc.is_null("distance") {
+						None
+					} else {
+						Some(doc.get_f64("distance")? as f32)
+					},
 				})
 			})
 			.try_collect::<Vec<Result<SchoolListing, ValueAccessError>>>()
@@ -116,6 +97,6 @@ pub async fn schools_list(
 			.map_err(to_unexpected!("Reading list of schools cursor failed"))?
 			.into_iter()
 			.collect::<Result<Box<[SchoolListing]>, _>>()
-			.map_err(to_unexpected!("Deserializing school listing failed"))?
+			.map_err(to_unexpected!("Deserializing school listing failed"))?,
 	)
 }
--- src/services/posts.rs
+++ src/services/posts.rs
@@ -1,58 +1,22 @@
 use std::convert::TryFrom;
 
-use actix_web::{
-	get,
-	post,
-	put,
-};
 use actix_web::web;
-use futures::{
-	TryStreamExt,
-};
-use log::{
-	debug,
-	info,
-	error,
-};
-use mongodb::{
-	Client as MongoClient,
-	Database,
-};
-use mongodb::bson::{
-	DateTime,
-	Document,
-	doc,
-};
-use mongodb::error::{
-	ErrorKind,
-	WriteFailure,
-};
-use mongodb::options::{
-	FindOptions,
-	FindOneOptions,
-};
+use actix_web::{get, post, put};
+use futures::TryStreamExt;
+use log::{debug, error, info};
+use mongodb::bson::oid::ObjectId;
+use mongodb::bson::{doc, DateTime, Document};
+use mongodb::error::{ErrorKind, WriteFailure};
+use mongodb::options::{FindOneOptions, FindOptions};
+use mongodb::{Client as MongoClient, Database};
 use serde::{Deserialize, Serialize};
 
-use crate::{
-	to_unexpected,
-};
+use crate::api_types::{failure, success, ApiResult, Failure};
 use crate::auth::AuthenticatedUser;
-use crate::api_types::{
-	ApiResult,
-	Failure,
-	success,
-};
 use crate::conf;
-use crate::masked_oid::{
-	self,
-	MaskingKey,
-	MaskedObjectId,
-	MaskedSequentialId,
-};
-use crate::types::{
-	Post,
-	Vote,
-};
+use crate::masked_oid::{self, MaskedObjectId, MaskedSequentialId, MaskingKey};
+use crate::to_unexpected;
+use crate::types::{Post, Vote};
 
 #[derive(Serialize)]
 pub struct ReplyContext {
@@ -78,9 +42,7 @@ pub struct Detail {
 #[derive(Deserialize)]
 #[serde(tag = "sort", rename_all = "kebab-case")]
 pub enum ListQuery {
-	Recent {
-		before: Option<MaskedSequentialId>,
-	},
+	Recent { before: Option<MaskedSequentialId> },
 	Trending,
 }
 
@@ -94,6 +56,86 @@ pub struct Created {
 	pub id: MaskedObjectId,
 }
 
+//* MATT
+
+#[derive(Deserialize)]
+#[serde(tag = "sort", rename_all = "kebab-case")]
+pub enum HottestQuery {
+	PastDate { date: DateTime },
+	Today,
+}
+
+#[get("/hottest/")]
+pub async fn daily_hottest(
+	db: web::Data<Database>,
+	masking_key: web::Data<&'static MaskingKey>,
+	query: web::Query<HottestQuery>,
+) -> ApiResult<Box<[Detail]>, ()> {
+	let sort: Document = doc! {"absolute_score": -1}; // What qualifies a post as "hottest" can change in the future
+	let find_query: Document;
+	match &*query {
+		HottestQuery::PastDate { date } => {
+			// TODO: if date is in future
+			if (true) {
+				return Err(Failure::BadRequest("date can't be in the future"));
+			}
+			find_query = doc! {
+				"$and": [
+					{"created_date": {"$gt": "OLDEST_DATE_BASED_ON_PASSED_DATE"}},
+					{"created_date": {"$lt": "NEWEST_DATE_BASED_ON_PASSED_DATE"}},
+				]
+			};
+		}
+		HottestQuery::Today => {
+			find_query = doc! {
+				"$and": [
+					{"created_date": {"$gt": "OLDEST_DATE"}},
+					{"created_date": {"$lt": "NEWEST_DATE"}},
+				]
+			};
+		}
+	}
+
+	let posts = db
+		.collection::<Post>("posts")
+		.find(
+			find_query,
+			FindOptions::builder()
+				.sort(sort)
+				.limit(i64::from(conf::HOTTEST_POSTS_PER_PAGE))
+				.build(),
+		)
+		.await
+		.map_err(to_unexpected!("Getting posts cursor failed"))?
+		.map_ok(|post| {
+			Ok(Detail {
+				id: masking_key.mask(&post.id),
+				sequential_id: masking_key
+					.mask_sequential(u64::try_from(post.sequential_id).unwrap()),
+				reply_context: None,
+				text: post.text,
+				created_at: (post
+					.id
+					.timestamp()
+					.try_to_rfc3339_string()
+					.map_err(to_unexpected!("Formatting post timestamp failed"))?),
+				votes: Votes {
+					up: u32::try_from(post.votes_up).unwrap(),
+					down: u32::try_from(post.votes_down).unwrap(),
+				},
+			})
+		})
+		.try_collect::<Vec<Result<Detail, Failure<()>>>>()
+		.await
+		.map_err(to_unexpected!("Getting posts failed"))?
+		.into_iter()
+		.collect::<Result<Vec<Detail>, Failure<()>>>()?;
+
+	success(posts.into())
+}
+
+//* MATT
+
 #[get("/posts/")]
 pub async fn list(
 	db: web::Data<Database>,
@@ -102,15 +144,14 @@ pub async fn list(
 ) -> ApiResult<Box<[Detail]>, ()> {
 	let find_query;
 	let sort;
-
 	match &*query {
 		ListQuery::Recent { before } => {
 			find_query = match before {
 				None => doc! {},
 				Some(before) => {
-					let before =
-						masking_key.unmask_sequential(before)
-						.map_err(|masked_oid::PaddingError| Failure::BadRequest("bad masked sequential id"))?;
+					let before = masking_key.unmask_sequential(before).map_err(
+						|masked_oid::PaddingError| Failure::BadRequest("bad masked sequential id"),
+					)?;
 					doc! {
 						"sequential_id": {"$lt": i64::try_from(before).unwrap()},
 					}
@@ -125,44 +166,48 @@ pub async fn list(
 		}
 	}
 
-	let posts =
-		db.collection::<Post>("posts")
-			.find(
-				find_query,
-				FindOptions::builder()
-					.sort(sort)
-					.limit(i64::from(conf::POSTS_PAGE_SIZE))
-					.build()
-			)
-			.await
-			.map_err(to_unexpected!("Getting posts cursor failed"))?
-			.map_ok(|post| Ok(Detail {
+	let posts = db
+		.collection::<Post>("posts")
+		.find(
+			find_query,
+			FindOptions::builder()
+				.sort(sort)
+				.limit(i64::from(conf::POSTS_PAGE_SIZE))
+				.build(),
+		)
+		.await
+		.map_err(to_unexpected!("Getting posts cursor failed"))?
+		.map_ok(|post| {
+			Ok(Detail {
 				id: masking_key.mask(&post.id),
-				sequential_id: masking_key.mask_sequential(u64::try_from(post.sequential_id).unwrap()),
+				sequential_id: masking_key
+					.mask_sequential(u64::try_from(post.sequential_id).unwrap()),
 				reply_context: None,
 				text: post.text,
-				created_at: (
-					post.id.timestamp()
-						.try_to_rfc3339_string()
-						.map_err(to_unexpected!("Formatting post timestamp failed"))?
-				),
+				created_at: (post
+					.id
+					.timestamp()
+					.try_to_rfc3339_string()
+					.map_err(to_unexpected!("Formatting post timestamp failed"))?),
 				votes: Votes {
 					up: u32::try_from(post.votes_up).unwrap(),
 					down: u32::try_from(post.votes_down).unwrap(),
 				},
-			}))
-			.try_collect::<Vec<Result<Detail, Failure<()>>>>()
-			.await
-			.map_err(to_unexpected!("Getting posts failed"))?
-			.into_iter()
-			.collect::<Result<Vec<Detail>, Failure<()>>>()?;
+			})
+		})
+		.try_collect::<Vec<Result<Detail, Failure<()>>>>()
+		.await
+		.map_err(to_unexpected!("Getting posts failed"))?
+		.into_iter()
+		.collect::<Result<Vec<Detail>, Failure<()>>>()?;
 
 	success(posts.into())
 }
 
 /// Gets the time-based offset of the trending score for the given timestamp.
 fn get_trending_score_time(date_time: &DateTime) -> f64 {
-	f64::from(u32::try_from(date_time.timestamp_millis() / 1000 - conf::TRENDING_EPOCH).unwrap()) / conf::TRENDING_DECAY
+	f64::from(u32::try_from(date_time.timestamp_millis() / 1000 - conf::TRENDING_EPOCH).unwrap())
+		/ conf::TRENDING_DECAY
 }
 
 #[post("/posts/")]
@@ -192,18 +237,20 @@ pub async fn create(
 			return Err(Failure::Unexpected);
 		}
 
-		let last_sequential_id =
-			db.collection::<Post>("posts")
+		let last_sequential_id = db
+			.collection::<Post>("posts")
 			.aggregate(
 				[
 					doc! {"$sort": {"sequential_id": -1}},
 					doc! {"$limit": 1},
 					doc! {"$project": {"_id": false, "sequential_id": true}},
 				],
-				None
+				None,
 			)
 			.await
-			.map_err(to_unexpected!("Getting next post sequential id cursor failed"))?
+			.map_err(to_unexpected!(
+				"Getting next post sequential id cursor failed"
+			))?
 			.try_next()
 			.await
 			.map_err(to_unexpected!("Getting next post sequential id failed"))?
@@ -212,27 +259,29 @@ pub async fn create(
 		let new_sequential_id = last_sequential_id.unwrap_or(0) + 1;
 		insert_doc.insert("sequential_id", new_sequential_id);
 
-		match db.collection::<Document>("posts").insert_one(&insert_doc, None).await {
+		match db
+			.collection::<Document>("posts")
+			.insert_one(&insert_doc, None)
+			.await
+		{
 			Ok(insertion) => break insertion,
-			Err(err) => {
-				match err.kind.as_ref() {
-					ErrorKind::Write(WriteFailure::WriteError(write_err)) if write_err.code == 11000 => {
-						info!("Retrying post creation: {}", err);
-					}
-					_ => {
-						error!("Creating post failed: {}", err);
-						return Err(Failure::Unexpected);
-					}
+			Err(err) => match err.kind.as_ref() {
+				ErrorKind::Write(WriteFailure::WriteError(write_err))
+					if write_err.code == 11000 =>
+				{
+					info!("Retrying post creation: {}", err);
 				}
-			}
+				_ => {
+					error!("Creating post failed: {}", err);
+					return Err(Failure::Unexpected);
+				}
+			},
 		}
 	};
 
-	success(
-		Created {
-			id: masking_key.mask(&insertion.inserted_id.as_object_id().unwrap()),
-		}
-	)
+	success(Created {
+		id: masking_key.mask(&insertion.inserted_id.as_object_id().unwrap()),
+	})
 }
 
 #[put("/posts/{post_id}/vote")]
@@ -242,16 +291,18 @@ pub async fn vote(
 	masking_key: web::Data<&'static MaskingKey>,
 	user: AuthenticatedUser,
 	post_id: web::Path<MaskedObjectId>,
-	request: web::Json<i32>,  // TODO: enum; see https://github.com/serde-rs/serde/issues/745
+	request: web::Json<i32>, // TODO: enum; see https://github.com/serde-rs/serde/issues/745
 ) -> ApiResult<Votes, ()> {
 	if !(-1..=1).contains(&*request) {
 		return Err(Failure::BadRequest("invalid vote"));
 	}
 
-	let post_id = masking_key.unmask(&post_id)
+	let post_id = masking_key
+		.unmask(&post_id)
 		.map_err(|masked_oid::PaddingError| Failure::BadRequest("bad masked id"))?;
 
-	let mut session = mongo_client.start_session(None)
+	let mut session = mongo_client
+		.start_session(None)
 		.await
 		.map_err(to_unexpected!("Starting session failed"))?;
 
@@ -264,39 +315,45 @@ pub async fn vote(
 		}
 
 		if attempt > 1 {
-			session.abort_transaction()
+			session
+				.abort_transaction()
 				.await
 				.map_err(to_unexpected!("Aborting vote transaction failed"))?;
 		}
 
-		let existing_vote = db.collection::<Vote>("votes").find_one_with_session(
-			doc! {
-				"post": {"$eq": post_id},
-				"user": {"$eq": user.id},
-			},
-			None,
-			&mut session
-		)
+		let existing_vote = db
+			.collection::<Vote>("votes")
+			.find_one_with_session(
+				doc! {
+					"post": {"$eq": post_id},
+					"user": {"$eq": user.id},
+				},
+				None,
+				&mut session,
+			)
 			.await
 			.map_err(to_unexpected!("Finding existing vote failed"))?
 			.map(|v| v.value);
 
-		session.start_transaction(None)
+		session
+			.start_transaction(None)
 			.await
 			.map_err(to_unexpected!("Starting transaction failed"))?;
 
 		match existing_vote {
 			None => {
-				match
-					db.collection::<Vote>("votes").insert_one_with_session(
+				match db
+					.collection::<Vote>("votes")
+					.insert_one_with_session(
 						Vote {
 							post: post_id,
 							user: user.id,
 							value: *request,
 						},
 						None,
-						&mut session
-					).await
+						&mut session,
+					)
+					.await
 				{
 					Ok(_) => {}
 					Err(err) => {
@@ -306,8 +363,9 @@ pub async fn vote(
 				}
 			}
 			Some(existing_vote) => {
-				match
-					db.collection::<Vote>("votes").update_one_with_session(
+				match db
+					.collection::<Vote>("votes")
+					.update_one_with_session(
 						doc! {
 							"post": {"$eq": post_id},
 							"user": {"$eq": user.id},
@@ -319,8 +377,9 @@ pub async fn vote(
 							},
 						},
 						None,
-						&mut session
-					).await
+						&mut session,
+					)
+					.await
 				{
 					Ok(update_result) if update_result.matched_count == 1 => {}
 					Ok(_) => {
@@ -336,50 +395,53 @@ pub async fn vote(
 		}
 
 		let votes_up_difference = -i32::from(existing_vote == Some(1)) + i32::from(*request == 1);
-		let votes_down_difference = -i32::from(existing_vote == Some(-1)) + i32::from(*request == -1);
+		let votes_down_difference =
+			-i32::from(existing_vote == Some(-1)) + i32::from(*request == -1);
 		let difference = -existing_vote.unwrap_or(0) + *request;
 
 		let trending_score_time = get_trending_score_time(&post_id.timestamp());
 
 		// TODO: Are update pipelines atomic? I haven’t found a straight answer yet.
-		let post_update = db.collection::<Post>("posts").update_one_with_session(
-			doc! {
-				"_id": {"$eq": post_id},
-			},
-			vec! [
+		let post_update = db
+			.collection::<Post>("posts")
+			.update_one_with_session(
 				doc! {
-					"$addFields": {
-						"votes_up": {
-							"$add": ["$votes_up", {"$literal": votes_up_difference}],
-						},
-						"votes_down": {
-							"$add": ["$votes_down", {"$literal": votes_down_difference}],
-						},
-						"absolute_score": {
-							"$add": ["$absolute_score", {"$literal": difference}],
+					"_id": {"$eq": post_id},
+				},
+				vec![
+					doc! {
+						"$addFields": {
+							"votes_up": {
+								"$add": ["$votes_up", {"$literal": votes_up_difference}],
+							},
+							"votes_down": {
+								"$add": ["$votes_down", {"$literal": votes_down_difference}],
+							},
+							"absolute_score": {
+								"$add": ["$absolute_score", {"$literal": difference}],
+							},
 						},
 					},
-				},
-				doc! {
-					"$addFields": {
-						"trending_score": {"$add": [
-							{"$multiply": [
-								{"$cond": [
-									{"$lt": ["$absolute_score", 0]},
-									-1,
-									1,
+					doc! {
+						"$addFields": {
+							"trending_score": {"$add": [
+								{"$multiply": [
+									{"$cond": [
+										{"$lt": ["$absolute_score", 0]},
+										-1,
+										1,
+									]},
+									{"$ln":
+										{"$add": [1, {"$abs": "$absolute_score"}]}},
 								]},
-								{"$ln":
-									{"$add": [1, {"$abs": "$absolute_score"}]}},
+								{"$literal": trending_score_time},
 							]},
-							{"$literal": trending_score_time},
-						]},
+						},
 					},
-				},
-			],
-			None,
-			&mut session
-		)
+				],
+				None,
+				&mut session,
+			)
 			.await
 			.map_err(to_unexpected!("Updating post score failed"))?;
 
@@ -396,18 +458,20 @@ pub async fn vote(
 		break;
 	}
 
-	let votes = db.collection::<Votes>("posts").find_one(
-		doc! {
-			"_id": {"$eq": post_id},
-		},
-		FindOneOptions::builder()
-			.projection(doc! {
-				"_id": false,
-				"up": "$votes_up",
-				"down": "$votes_down",
-			})
-			.build()
-	)
+	let votes = db
+		.collection::<Votes>("posts")
+		.find_one(
+			doc! {
+				"_id": {"$eq": post_id},
+			},
+			FindOneOptions::builder()
+				.projection(doc! {
+					"_id": false,
+					"up": "$votes_up",
+					"down": "$votes_down",
+				})
+				.build(),
+		)
 		.await
 		.map_err(to_unexpected!("Retrieving updated post votes failed"))?
 		.ok_or_else(|| {
