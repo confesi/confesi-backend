2dac8749972966a1fb0694c20209a1217b36db8e
Matthew <matthew.rl.trent@gmail.com>
2023-04-09 04:29:46 -0700

    WIP: adds verification route by clicking on link sent to email, without the actual email working yet

--- src/services/email.rs
+++ src/services/email.rs
@@ -6,14 +6,15 @@ use regex::Regex;
 use crate::{
 	api_types::{success, ApiResult, Failure},
 	auth::AuthenticatedUser,
+	conf::HOST,
 	masked_oid::{self, MaskedObjectId, MaskingKey},
 	to_unexpected,
 	types::{School, User},
 };
-use actix_web::{delete, get, post, put, web};
+use actix_web::{get, post, web};
 use jsonwebtoken::{decode, encode, errors::Error, DecodingKey, EncodingKey, Header, Validation};
 use mongodb::Database;
-use serde::{de::Expected, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Claims {
@@ -113,7 +114,7 @@ pub async fn send_verification_email(
 					|masked_oid::PaddingError| Failure::BadRequest("bad masked sequential id"),
 				)?,
 			);
-			success(token)
+			success(format!("http://{}/verify/{}/", HOST, token)) // todo: send email here
 		}
 		Err(err) => {
 			error!("Error creating JWT: {}", err);
@@ -122,8 +123,51 @@ pub async fn send_verification_email(
 	}
 }
 
+// todo: return simple HTML disclosing results?
 #[get("/verify/{token}/")]
-pub async fn verify_link(db: web::Data<Database>, user: AuthenticatedUser) -> ApiResult<(), ()> {
-	// todo: when verifying, do one last check to ensure the email isn't already in use (potential race condition between 2 people verifying the same email)
+pub async fn verify_link(
+	db: web::Data<Database>,
+	token: web::Path<String>,
+	masking_key: web::Data<&'static MaskingKey>,
+) -> ApiResult<(), ()> {
+	// todo: when verifying, do one last check to ensure the email isn't already in use (potential race condition between 2 people verifying the same email). does it need to be atomic?
+	let claims = Claims::decode_jwt(&token).map_err(|_| Failure::BadRequest("invalid token"))?;
+	let user_id = masking_key
+		.unmask(&claims.masked_user_id)
+		.map_err(|masked_oid::PaddingError| Failure::BadRequest("bad masked sequential id"))?;
+
+	// is the email already in use?
+	let potential_user = db
+		.collection::<User>("users")
+		.find_one(
+			doc! {
+				"email": &claims.email
+			},
+			None,
+		)
+		.await
+		.map_err(to_unexpected!("finding a user with this email failed"))?;
+
+	if let Some(_) = potential_user {
+		return Err(Failure::BadRequest("email already in use"));
+	}
+
+	// update user with verified and email_verified
+	db.collection::<User>("users")
+		.update_one(
+			doc! {
+				"_id": user_id
+			},
+			doc! {
+				"$set": {
+					"email": claims.email,
+					"email_verified": true,
+				}
+			},
+			None,
+		)
+		.await
+		.map_err(to_unexpected!("updating user's email failed"))?;
+
 	success(())
 }
